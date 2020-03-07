119c8679dc5d51e95010df9de667b957e08e6272
sadrettincali <sadrettincali@gmail.com>
2020-03-07 01:56:27 -0500

    New Commit

--- src/test/java/tests/MyFirstProject.java
+++ src/test/java/tests/MyFirstProject.java
@@ -10,19 +10,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import utilities.DriverFactory;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MyFirstProject {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().version("79").setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://amazon.com");
         System.out.println(driver.getTitle());
         driver.navigate().to("http:google.com");
         System.out.println(driver.getTitle());
         driver.navigate().back();
         driver.findElement(By.linkText("Customer Service")).click();
-        driver.findElement(By.linkText(" Change Your Payment Method ")).click();
         driver.quit();
 
     }
